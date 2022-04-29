9733c09ef95cfcf9779601bf501e3c323a4c4899
Giko <giko.ffxi@gmail.com>
2022-04-29 00:44:32 -0400

    Initial commit

---
+++ giko-cache.lua
@@ -0,0 +1,7 @@
+_addon.author 	= 'giko'
+_addon.name 	= 'giko-cache'
+_addon.version 	= '1.0.0'
+
+print("-= This is a storage cache location for the giko's addons. There is no need to load it. =-")
+
+AshitaCore:GetChatManager():QueueCommand(string.format('/addon unload giko-cache'), 1)    
